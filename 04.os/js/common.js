2a582250c4037893e2cf30d12dba3ce070c1e068
angryjamu <angryjamu@naver.com>
2024-02-19 17:53:38 +0900

    k

--- 04.os/js/common.js
+++ 04.os/js/common.js
@@ -1,26 +1,27 @@
 var swiper = new Swiper(".mySwiper", {
-    loop:true,
-    effect: "fade",
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    }
-  });
+  loop: true,
+  autoplay: true,
+  effect: "fade",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }
+});
 
-  var swiper2 = new Swiper(".prd_list_slide", {
-    loop:true,
-    autoplay: {
-      delay: 3500,
-      disableOnInteraction: false,
-    },
-    slidesPerView: 5,
-    spaceBetween: 5,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    }
-  });
+var swiper2 = new Swiper(".prd_list_slide", {
+  loop: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 5,
+  spaceBetween: 5,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+});
