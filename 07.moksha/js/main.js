64468cc59272efc9d15c0c8740f4850523278b1e
kangboseong <158239745+angryjamu@users.noreply.github.com>
2024-04-29 22:38:49 +0900

    k

--- 07.moksha/js/main.js
+++ 07.moksha/js/main.js
@@ -11,3 +11,20 @@ var swiper = new Swiper(".swiper-fade", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  window.addEventListener('scroll', function() {
+    // 스크롤한 높이가 300px를 초과하면 배경색 변경
+    if (window.scrollY > 7300) {
+        // 배경색 변경
+        document.body.style.backgroundColor = '#302833';
+        // #header와 #footer에 filter 속성 적용
+        document.getElementById('header').style.filter = 'invert(100%)';
+        document.getElementById('footer').style.filter = 'invert(100%)';
+    } else {
+        // 스크롤이 300px 미만인 경우 배경색을 초기값으로 변경
+        document.body.style.backgroundColor = '';
+        // #header와 #footer의 filter 속성 초기화
+        document.getElementById('header').style.filter = 'none';
+        document.getElementById('footer').style.filter = 'none';
+    }
+});
