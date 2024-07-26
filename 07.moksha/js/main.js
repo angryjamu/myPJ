6000d5871cd22eeee97abc0d14d17bb3033798ea
kangboseong <158239745+angryjamu@users.noreply.github.com>
2024-07-26 23:43:29 +0900

    k

--- 07.moksha/js/main.js
+++ 07.moksha/js/main.js
@@ -1,5 +1,4 @@
 
-
   window.addEventListener('scroll', function() {
     let scrollThreshold = window.innerWidth < 768 ? 5500 : 7310;
 
@@ -8,6 +7,27 @@
     } else {
         document.body.classList.remove('scrolled');
     }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const inputs = document.querySelectorAll('.seasonal-category');
+    const menus = document.querySelectorAll('.menu-list-wrapper');
+
+    function showMenu() {
+        menus.forEach(menu => {
+            menu.style.display = 'none';
+        });
+
+        const activeMenuId = document.querySelector('.seasonal-category:checked').value + '-menu';
+        document.getElementById(activeMenuId).style.display = 'block';
+    }
+
+    inputs.forEach(input => {
+        input.addEventListener('change', showMenu);
+    });
+
+    // 초기 로딩 시 현재 체크된 메뉴를 보여줌
+    showMenu();
 });
 
 // wowJS
