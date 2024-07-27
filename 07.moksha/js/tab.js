a5c1e57c93aa6ba812e87e8af36117da0b190cb7
kangboseong <158239745+angryjamu@users.noreply.github.com>
2024-07-27 12:37:57 +0900

    k

--- 07.moksha/js/tab.js
+++ 07.moksha/js/tab.js
@@ -18,3 +18,22 @@ document.addEventListener('DOMContentLoaded', function () {
     // 초기 로딩 시 현재 체크된 메뉴를 보여줌
     showMenu();
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const detailButton = document.getElementById('attention-detail-button');
+    const explanationBox = document.getElementById('explanation-box');
+
+    detailButton.addEventListener('click', function () {
+        if (explanationBox.style.display === 'none' || explanationBox.style.display === '') {
+            explanationBox.style.display = 'flex';
+            setTimeout(() => {
+                explanationBox.style.opacity = '1';
+            }, 10);  // 약간의 지연을 주어 transition이 적용되도록 함
+        } else {
+            explanationBox.style.opacity = '0';
+            setTimeout(() => {
+                explanationBox.style.display = 'none';
+            }, 400);  // CSS transition 시간과 동일하게 설정
+        }
+    });
+});
